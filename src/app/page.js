f6ae4cafc88ca07e43a39bd98b6570166a55c069
heyheynic <nicsayhey@gmail.com>
2024-11-11 14:35:25 +0100

    index page.js styling for easy access

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,10 @@ import Link from "next/link";
 import PrimaryButton from "@/components/PrimaryButton";
 
 export default function Home() {
-  return <div>
+  return <div className="h-screen grid place-content-center bg-teal-300">
+    <h1 className="text-4xl font-bold font-sans">Greatest online shopping experience!</h1>
     <Link href={`./products/page.jsx`}>
-    <PrimaryButton />
+    <PrimaryButton btnText="Go to products"/>
     </Link>
   </div>;
 }
--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -1,5 +1,5 @@
-const PrimaryButton = () => {
-    return <div className="p-4 border-2 w-fit rounded-md border-black hover:bg-black hover:text-white">Button</div>;
+const PrimaryButton = ({btnText}) => {
+    return <div className="p-4 border-2 w-fit rounded-md border-black hover:bg-black hover:text-white">{btnText}</div>;
 }
  
 export default PrimaryButton;
