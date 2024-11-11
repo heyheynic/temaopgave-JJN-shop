47e4f74c2c6348a2aa6545445d6ecf6422dec636
Jules KJ <juljen2730@gmail.com>
2024-11-11 15:01:58 +0100

    branch payment site start up

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -1,5 +1,26 @@
+import Image from "next/image";
+
 const Page = async () => {
-  return <div></div>;
+  return (
+    <div>
+      <section>
+        <div>
+          <Image
+            src="/faveicon.ico"
+            width={200}
+            height={200}
+            alt="placeholder image"
+          ></Image>
+          <h3>Tittle</h3>
+          <p>Pris</p>
+        </div>
+      </section>
+      <section className="flex flex-col items-center justify-center">
+        <p>Du skal betale i alt </p>
+        <p>Placeholder Button</p>
+      </section>
+    </div>
+  );
 };
 
 export default Page;
