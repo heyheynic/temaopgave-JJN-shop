fc8b8dfad099692599613cddbbf30ca560a0756e
Jules KJ <juljen2730@gmail.com>
2024-11-11 14:33:38 +0100

    very simple header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,38 @@
+import Link from "next/link";
+
 const Header = () => {
-    return <div></div>;
-}
- 
+  return (
+    <header className="py-4 px-2 border-b-2 border-black">
+      <nav>
+        <ul className="flex flex-row items-center justify-start gap-2 ">
+          <li>
+            <Link
+              className="bg-black text-white py-2 px-4 rounded-full outline outline-black outline-1 hover:outline-offset-2 focus:outline-offset-2 capitalize"
+              href={`/`}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="bg-black text-white py-2 px-4 rounded-full outline outline-black outline-1  hover:outline-offset-2 focus:outline-offset-2 capitalize"
+              href={`/payment`}
+            >
+              Basket
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="bg-black text-white py-2 px-4 rounded-full outline outline-black outline-1  hover:outline-offset-2 focus:outline-offset-2 capitalize"
+              href={`/products`}
+            >
+              Product List
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
 export default Header;
