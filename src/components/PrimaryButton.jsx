4c9bba8a25c0bc50312846c35b92baadd868e0ea
heyheynic <nicsayhey@gmail.com>
2024-11-11 14:35:05 +0100

    bsic styling of button and index

--- src/components/PrimaryButton.jsx
+++ src/components/PrimaryButton.jsx
@@ -1,5 +1,5 @@
 const PrimaryButton = () => {
-    return <div>Button</div>;
+    return <div className="p-4 border-2 w-fit rounded-md border-black hover:bg-black hover:text-white">Button</div>;
 }
  
 export default PrimaryButton;
