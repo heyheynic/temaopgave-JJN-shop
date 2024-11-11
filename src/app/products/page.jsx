9c8953b37dfdb21c53ca9f3f100a74d4893b41ef
Julie <julieglion@gmail.com>
2024-11-11 14:27:45 +0100

    Add initial setup

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,5 +1,25 @@
+import Basket from "@/components/Basket";
+import CategoryFilter from "@/components/CategoryFilter";
+import ProductCard from "@/components/ProductCard";
+import SearchBar from "@/components/SearchBar";
+
 const Page = async () => {
-  return <div>hej</div>;
+  return (
+    <div>
+      <h1>Products</h1>
+      <div>
+        <CategoryFilter />
+        <SearchBar />
+        <p>
+          <span>0</span>items
+        </p>
+        <div>
+          <ProductCard />
+        </div>
+      </div>
+      <Basket />
+    </div>
+  );
 };
 
 export default Page;
