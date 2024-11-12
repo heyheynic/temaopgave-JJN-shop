4ddc7951ce82bc7b3e92511a2d71f191b36d5fdb
Jules KJ <juljen2730@gmail.com>
2024-11-12 13:35:12 +0100

    antal replaced

--- src/components/PaymentProductCard.jsx
+++ src/components/PaymentProductCard.jsx
@@ -4,7 +4,7 @@ const PaymentProductCard = ({ id, src, productTitle, price }) => {
   return (
     <li
       key={id}
-      className="flex flex-row border-2 border-[--redwood] rounded-md max-w-[65ch] "
+      className="flex flex-row justify-start border-2 border-[--redwood] rounded-md max-w-[65ch] "
     >
       <Image src={src} width={100} height={100} alt="placeholder image"></Image>
       <div>
@@ -12,7 +12,7 @@ const PaymentProductCard = ({ id, src, productTitle, price }) => {
 
         <p className="text-text">{price}$</p>
       </div>
-      <span className="text-text">antal</span>
+      <span className="text-text ml-[auto] px-2">antal</span>
     </li>
   );
 };
