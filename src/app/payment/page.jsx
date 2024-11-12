12c370a455e313ae126d4270740fe9c448c9ebb5
Jules KJ <juljen2730@gmail.com>
2024-11-12 13:26:03 +0100

    bruger reduce() til at calkulere summen af alle elementer

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -2,12 +2,14 @@
 import Image from "next/image";
 import PrimaryButton from "@/components/PrimaryButton";
 
+import PaymentProductCard from "@/components/PaymentProductCard";
+
 import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const Page = () => {
   const { data, error, isLoading } = useSWR(
-    "https://dummyjson.com/products/1",
+    "https://dummyjson.com/products",
     fetcher
   );
 
@@ -15,46 +17,33 @@ const Page = () => {
 
   if (error) return <div>failed to load</div>;
   if (isLoading) return <div>loading...</div>;
+
+  //virker kun lige nu for fetchen
+  const total = data.products.reduce(
+    (accumulator, product) => accumulator + product.price,
+    0
+  );
+
   return (
     <div>
-      <h1 className="text-4xl text-center sm:text-left capitalize py-4 px-1">
-        {" "}
+      <h1 className="text-title text-center sm:text-left capitalize py-4 px-1">
         Your basket
       </h1>
 
-      <section className="grid grid-rows-[auto] gap-2  py-2 px-1 ">
-        <div className="flex flex-row border-2 border-black max-w-[65ch]">
-          <Image
-            src={data.thumbnail}
-            width={100}
-            height={100}
-            alt="placeholder image"
-          ></Image>
-          <div>
-            <h3>{data.title}</h3>
-            <span>antal</span>
-            <p>{data.price}$</p>
-          </div>
-        </div>
-
-        <div className="flex flex-row border-2 border-black max-w-[65ch]">
-          <Image
-            src={data.thumbnail}
-            width={100}
-            height={100}
-            alt="placeholder image"
-          ></Image>
-          <div>
-            <h3>{data.title}</h3>
-            <span>antal</span>
-            <p>{data.price}$</p>
-          </div>
-        </div>
-      </section>
-      <section className="flex flex-col items-center justify-center text-center">
-        <p>
-          Your total is : <span>total</span>
-        </p>
+      <ul className="grid grid-rows-[auto] gap-2  py-2 px-1 ">
+        {data.products.map((product) => [
+          <PaymentProductCard
+            id={product.id}
+            src={product.thumbnail}
+            productTitle={product.title}
+            price={product.price}
+          ></PaymentProductCard>,
+        ])}
+      </ul>
+      <section className="text-sub-subtitle flex flex-col items-center justify-center text-center">
+        <h4>
+          Your total is : <span>${total}</span>
+        </h4>
         <PrimaryButton theme="black" btnText="Pay"></PrimaryButton>
       </section>
     </div>
--- src/components/PaymentProductCard.jsx
+++ src/components/PaymentProductCard.jsx
@@ -1,5 +1,20 @@
-const PaymentProductCard = () => {
-    return <div></div>;
-}
- 
+import Image from "next/image";
+
+const PaymentProductCard = ({ id, src, productTitle, price }) => {
+  return (
+    <li
+      key={id}
+      className="flex flex-row border-2 border-[--redwood] rounded-md max-w-[65ch] "
+    >
+      <Image src={src} width={100} height={100} alt="placeholder image"></Image>
+      <div>
+        <h3 className="text-sub-subtitle">{productTitle}</h3>
+
+        <p className="text-text">{price}$</p>
+      </div>
+      <span className="text-text">antal</span>
+    </li>
+  );
+};
+
 export default PaymentProductCard;
