f43d1f058e9a211a612ca3c633201271cf2c433c
heyheynic <nicsayhey@gmail.com>
2024-11-13 18:09:50 +0100

    added slug to url param

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -6,16 +6,26 @@ import Link from "next/link";
 import { IoIosArrowBack } from "react-icons/io";
 
 const Page = async ({ params }) => {
-  const id = (await params).id;
+  // const id = (await params).id;
+  // const id = params.id;
+
+  // Trying to give the URL a slug that is more readable while still including id
+
+  const idAndSlug = params.id;
+
+  // extracts only the id part (everything before the first hyphen)
+  const id = idAndSlug.split("-")[0];
 
   let response = await fetch(`https://dummyjson.com/products/${id}`);
   let data = await response.json();
+
+  console.log(data);
 
   return (
     <div className="max-w-[80dvw] m-auto my-m">
       <Link href={`../products/`} className="flex items-center gap-2 mb-m">
         <IoIosArrowBack />
-        Tilbage
+        Go back
       </Link>
       <ProductSingle product={data} />
       <div className="border-b-2 my-l"></div>
